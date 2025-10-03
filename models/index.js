f91ce0d85576c00edce52daa02a8753a050b8c80
Pratham Pansare <145864145+Pratham2511@users.noreply.github.com>
2025-10-03 17:48:56 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,21 +3,35 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const config = require('../config/config');
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect,
-  logging: config.logging
-});
+// Get the current environment (default to development)
+const env = process.env.NODE_ENV || 'development';
+// Get the configuration for the current environment
+const dbConfig = config[env];
+
+// Create Sequelize instance with the correct configuration
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+    logging: dbConfig.logging,
+    dialectOptions: dbConfig.dialectOptions || {}
+  }
+);
 
 const db = {};
 
+// Load all models
 fs.readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== 'index.js')
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
+// Set up associations
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
